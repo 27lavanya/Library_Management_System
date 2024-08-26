8a009bd0e08629e97ea79af1f9b274cfb84d725c
27lavanya <158753041+27lavanya@users.noreply.github.com>
2024-08-26 23:03:35 +0530

    Add files via upload

---
+++ Library_System/BookServiceInterface.java
@@ -0,0 +1,14 @@
+package Library_System;
+
+public interface BookServiceInterface {
+    void addBook();
+
+    void showAllBooks();
+
+    void showAllAvailableBooks();
+
+    void borrowBook();
+
+    void returnBook();
+}
+
